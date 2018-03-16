db89657f8495d45820648a1d0bdd11ffb4a26b7c
Andrei Lucaci <contact@andreilucaci.ro>
2018-03-17 00:14:31 +0300

    Added templating winform project + unity

---
+++ QMe.Templating/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QMe.Templating.Unity;
+using Unity;
+
+namespace QMe.Templating
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			var container = CreateContainer();
+
+			UnityConfiguration.RegisterTypes(container);
+
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(container.Resolve<TemplateManager>());
+		}
+
+		static IUnityContainer CreateContainer()
+		{
+			return new UnityContainer();
+		}
+	}
+}
---
+++ QMe.Templating/Unity/UnityConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Unity;
+
+namespace QMe.Templating.Unity
+{
+	public class UnityConfiguration
+	{
+		public static void RegisterTypes(IUnityContainer container)
+		{
+			container.RegisterType<TemplateManager>();
+		}
+	}
+}
